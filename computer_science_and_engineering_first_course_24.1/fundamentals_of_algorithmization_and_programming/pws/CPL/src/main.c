a91510a4673344072fa4feb1b10baa2309864e5d
FredNekrasov <frednekrasov49@gmail.com>
2024-10-27 14:40:54 +0300

    saving modified files

--- computer_science_and_engineering_first_course_24.1/fundamentals_of_algorithmization_and_programming/pws/CPL/src/main.c
+++ computer_science_and_engineering_first_course_24.1/fundamentals_of_algorithmization_and_programming/pws/CPL/src/main.c
@@ -1,108 +1,123 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <tgmath.h>
+#include <math.h>
 
-int* createIntArray(const int n) {
-    int* array = calloc(n, sizeof(int));
-    if (array == NULL) {
-        free(array);
+#define n 3
+
+struct InformationAboutEnterprises {
+    char* name;
+    int allEmployees;
+    int industrialEmployees;
+    int notIndustrialEmployees;
+    float notIndustrialEmployeesPercentage;
+    float planForEmployees;
+} typedef infAboutEnterprises;
+
+infAboutEnterprises* creatingAndFillingIAEs() {
+    infAboutEnterprises* enterprises = calloc(n, sizeof(infAboutEnterprises));
+    if (enterprises == NULL) {
+        free(enterprises);
         return nullptr;
     }
-    for (int i = 0; i < n; i++) array[i] = rand() % 10;
-    return array;
-}
-float* createFloatArray(const int n, int* array1, int* array2) {
-    float* array = calloc(n, sizeof(float));
-    if (array == NULL) {
-        free(array);
-        return nullptr;
+    for (int i = 0; i < n; i++) {
+        printf("Enterprise %d\n", i + 1);
+        printf("name:");
+        enterprises[i].name = calloc(20, sizeof(char));
+        if (enterprises[i].name == NULL) {
+            for (int j = 0; j <= i; j++) free(enterprises[i].name);
+            free(enterprises);
+            return nullptr;
+        }
+        scanf_s("%s", enterprises[i].name, 20);
+        printf("allEmployees:");
+        scanf_s("%d", &enterprises[i].allEmployees);
+        printf("industrialEmployees:");
+        scanf_s("%d", &enterprises[i].industrialEmployees);
+        enterprises[i].notIndustrialEmployees = enterprises[i].allEmployees - enterprises[i].industrialEmployees;
+        enterprises[i].notIndustrialEmployeesPercentage = (float)enterprises[i].notIndustrialEmployees / (float)enterprises[i].allEmployees;
+        enterprises[i].planForEmployees = (float)enterprises[i].industrialEmployees / (float)enterprises[i].allEmployees;
     }
-    for (int i = 0; i < n; i++) array[i] = rand() % 10;
-    return array;
+    return enterprises;
 }
-void outputIntArray(const int n, int array[]) {
-    for (int i = 0; i < n; i++) printf("%d ", array[i]);
+void outputIAE(const infAboutEnterprises enterprise, const int i) {
+    printf("| Enterprise %d |", i + 1);
+    printf(" name: %s |", enterprise.name);
+    printf(" allEmployees: %d |", enterprise.allEmployees);
+    printf(" industrialEmployees: %d |\n", enterprise.industrialEmployees);
+    printf("| Enterprise %d |", i + 1);
+    printf(" notIndustrialEmployees: %d |", enterprise.notIndustrialEmployees);
+    printf(" notIndustrialEmployeesPercentage: %.2f |", enterprise.notIndustrialEmployeesPercentage);
+    printf(" planForEmployees: %.2f |\n", enterprise.planForEmployees);
+    printf("------------------------\n");
 }
-void outputFloatArray(const int n, float array[]) {
-    for (int i = 0; i < n; i++) printf("%.3f ", array[i]);
-}
-bool isZero(const int n) {
-    return n == 0;
-}
-bool isEven(const int n) {
-    return n % 2 == 0;
-}
-float sumOfArrayElements(const float array[], const int n) {
-    float sum = 0;
-    for (int i = 0; i < n; i++) sum += array[i];
-    return sum;
-}
-float calculateMedian(float array[], const int n) {
-    // Сортировка первого массива для нахождения медианы
-    for (int i = 0; i < n - 1; i++) {
+char* smallestEnterprise(const infAboutEnterprises* enterprises) {
+    infAboutEnterprises iae = enterprises[0];
+    int iaeIndex = 0;
+    for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
-            if (array[i] > array[j]) {
-                const float temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
+            if (enterprises[i].planForEmployees < enterprises[j].planForEmployees) {
+                iaeIndex = i;
+                iae = enterprises[i];
             }
         }
     }
-    if (isEven(n)) return  (array[n / 2 - 1] + array[n / 2]) / 2;
-    return array[n / 2];
+    outputIAE(iae, iaeIndex);
+    return iae.name;
 }
-int fifthPW3Task() {
-    srand(3u);
-    int n = 0;
-    printf("input n: ");
-    scanf_s("%d", &n);
-    if (n > 13) return printf("error: n > 13");
-    int *array1 = createIntArray(n), *array2 = createIntArray(n);
-    if (array1 == NULL || array2 == NULL) {
-        free(array1);
-        free(array2);
-        return printf("error: array1 and array2 are NULL");
+int seventhPW3Task() {
+    infAboutEnterprises* enterprises = creatingAndFillingIAEs();
+    if (enterprises == NULL) {
+        free(enterprises);
+        return printf("error: enterprises pointer is NULL");
     }
-    printf("\narray1: ");
-    outputIntArray(n, array1);
-    printf("\narray2: ");
-    outputIntArray(n, array2);
-
-    float* array3 = calloc(n, sizeof(float));
-    if (array3 == NULL) {
-        free(array3);
-        return printf("error: array3 is NULL");
+    for (int i = 0; i < n; i++) outputIAE(enterprises[i], i);
+    printf("\n");
+    int choice = -1;
+    char* enterpriseName = enterprises[0].name;
+    while (choice != 0) {
+        printf("input i: ");
+        scanf_s("%d", &choice);
+        switch (choice) {
+            case 0:// запрашивать нижнюю границу процента выполнения плана по персоналу;
+                for (int i = 0; i < n; i++) {
+                    if (enterprises[i].planForEmployees < 0.5f) outputIAE(enterprises[i], i);
+                }
+                break;
+            case 1:// копировать из исходной в рабочую таблицу строки с процентом выполнения плана по персоналу, большим заданного;
+                printf("input i: ");
+                scanf_s("%d", &choice);
+                break;
+            case 2:// выявить предприятие с наименьшей долей непромышленного персонала и запоминать его наименование;
+                enterpriseName = smallestEnterprise(enterprises);
+                printf("%s\n", enterpriseName);
+                break;
+            case 3:
+                for (int i = 0; i < n; i++) outputIAE(enterprises[i], i);
+                break;
+            default:
+                break;
+        }
     }
-
-    printf("\narray3: ");
-    outputFloatArray(n, array3);
-    const float median = calculateMedian(array3, n);
-    printf("\nmedian: %.3f", median);
-    const float sum = sumOfArrayElements(array3, n);
-    const float average = sum / (float)n;
-    printf("\naverage: %.3f", average);
-
-    const float deviation = average - median;
-    printf("\ndeviation: %.3f", deviation);
-    free(array3);
+    free(enterpriseName);
+    for (int i = 0; i < n; i++) free(enterprises[i].name);
+    free(enterprises);
     return 0;
 }
 int main() {
     printf("Hello, World!\n");
-    fifthPW3Task();
     return 0;
 }
 
 void independentWork() {
     float y = 0;
     for (int a = 1; a <= 3; a += 1) {
-        float maxY = -1 * exp(24);
-        float maxX = 0;
-        float minY = exp(24);
-        float minX = 0;
+        double maxY = -1 * exp(24);
+        double maxX = 0;
+        double minY = exp(24);
+        double minX = 0;
         printf("a = %d\n", a);
-        for (float x = 0.5f; x < 3.5; x += 0.5f) {// здесь интервал меняем
-            y = sin(x) + fabsf(x) + pow(2, x);// здесь формулу поменять
+        for (double x = 0.5; x < 3.5; x += 0.5) {
+            y = sin(x) + fabs(x) + pow(2, x);
             printf("x = %.2f, y = %.2f\n", x, y);
             if (y > maxY) {
                 maxY = y;
